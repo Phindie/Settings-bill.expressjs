5123a26ef7246d674ba13730994fa58c33228ea2
Phindi <pn8256@gmail.com>
2018-08-15 12:53:58 +0200

    Modifications

---
+++ index.js
@@ -0,0 +1,76 @@
+const exphbs  = require('express-handlebars');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const SettingCost = require('./setting-bill')
+const calc = SettingCost();
+
+
+app.engine('handlebars',
+  exphbs({
+    defaultLayout: 'main',
+
+  }))
+
+app.set('view engine', 'handlebars');
+//bodyParser process or converting html data sent to the server
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+//accessesors
+app.get('/', function (req, res) {
+  let calls = calc.getCALL();
+  let smss = calc.getSMS();
+  let criticals = calc.critical();
+  let warnings = calc.warning();
+
+//getTotals
+  let callTotals = calc.callTotals().toFixed(2);
+  let smsTotals = calc.smsTotals().toFixed(2);
+  let grandTotal = calc.grandTotal();
+  let color = calc.myColor();
+
+  res.render('home',{grandTotal,callTotals,smsTotals,criticals,warnings,calls,smss,color});
+
+});
+
+//bill type
+ app.post('/action/', function (req, res){
+   //action = values of
+   let value = req.body.actionType;
+   calc.calculate(value);
+   calc.times(value);
+   res.redirect('/');
+
+ // console.log(action)
+});
+//displaying the timestamp
+//Modifiers
+app.post('/settings', function(req, res){
+  let calls = parseFloat(req.body.callCost);
+  let smss = parseFloat(req.body.smsCost);
+  let warnings = parseFloat(req.body.warningLevel);
+  let criticals = parseFloat(req.body.criticalLevel);
+  calc.setWarn(warnings);
+  calc.setCrit(criticals);
+  calc.setCall(calls);
+  calc.setSMS(smss);
+  // console.log(calc.getCALL());
+  res.redirect('/');
+});
+
+app.get('/actions', function (req, res){
+res.render('actions', {timestamp:calc.Stamps()});
+});
+
+app.get('/actions/:type', function (req, res){
+  let type = req.params.type;
+  calc.Stamps();
+res.render('actions', {timestamp:calc.filtering(type)});
+});
+
+  const PORT = process.env.PORT || 3007;
+  app.listen(PORT, function(){
+  console.log('App starting on port', PORT);
+});
---
+++ test/settingBill-test.js
@@ -0,0 +1,54 @@
+let assert = require("assert");
+let settingBill = require("../setting-bill");
+
+describe('SettingsBill Expressjs DOM function', function(){
+
+  it('Should return the total cost of the calls ', function(){
+         var setBill = settingBill();
+
+         setBill.callTotals("call");
+         setBill.callTotals("call");
+
+        assert.equal(setBill.getCALL(), 2.00);
+
+
+    });
+
+    it('If sms total does not reach critical should return false  ', function(){
+            var setting = settingBill();
+             setting.smsTotals(5.00);
+
+            setting.getSMS('sms');
+            setting.getSMS('sms');
+            setting.getSMS('sms');
+            setting.getSMS('sms');
+
+          assert.equal(setting.passesCritical(), false);
+
+});
+
+it('Should return the number of calls that are made ', function(){
+  var setting = settingBill();
+
+    setting.times('sms');
+    setting.times('sms');
+
+  assert.equal(setting.Stamps().length,2)
+
+   });
+it('Should return the time stamp of calls that are made ', function(){
+     var setting = settingBill();
+
+     setting.calculate('call');
+     setting.times('call');
+     let myDate = new Date();
+     assert.deepEqual(setting.Stamps(),[
+       {
+         type: 'call',
+         price: 2,
+         when: myDate,
+         ago : "a few seconds ago"
+       }
+     ]);
+});
+});
